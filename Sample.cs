1b7e7dd956c6fa72c06a8612c5c0754541a7f6ab
Yuvaraj Bharath Sundara Rajan <yuvaraj.sundara@itp.objectfrontier.com>
2023-09-13 22:07:06 +0530

    conditions added

--- Sample.cs
+++ Sample.cs
@@ -9,7 +9,16 @@
         Console.WriteLine("Enter the Marks ");
         Convert.ToInt32(Console.ReadLine());
        
-        
+        if(TotalMarks >= 90){
+          console.WriteLine("First mark");
+        }
+        elseif(TotalMarks > 70 && TotalMarks < 80){
+          console.WriteLine("Second mark");
+        }
+        else(TotalMarks < 40){
+          console.WriteLine("Fail");
+        }
+
           Console.WriteLine($"The {StudentName} has scored ..... ");
        
     }
