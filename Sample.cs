3311f0127e15b33ae7927598d4f0c26387b4591a
Sneha <67682184+SnehaBaskarann@users.noreply.github.com>
2023-09-13 21:16:18 +0530

    Create Sample.cs

---
+++ Sample.cs
@@ -0,0 +1,13 @@
+﻿public class Sample5{
+
+    public static void Main(string[] args){
+
+       string StudentName;
+       double TotalMarks;
+
+       
+        
+          Console.WriteLine($"The {StudentName} has scored ..... ");
+       
+    }
+}
